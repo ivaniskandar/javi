817c84f80e0d32e18c8a5093fa05c3baac73d4bf
Ivan Iskandar <iiiiskandar14@gmail.com>
2019-12-30 13:02:50 +0700

    Vibrate when turning off the screen
    Signed-off-by: Ivan Iskandar <iiiiskandar14@gmail.com>

--- app/src/main/java/xyz/ivaniskandar/javi/JaviService.kt
+++ app/src/main/java/xyz/ivaniskandar/javi/JaviService.kt
@@ -11,18 +11,26 @@ import android.content.Intent
 import android.content.Intent.ACTION_SCREEN_OFF
 import android.content.Intent.ACTION_SCREEN_ON
 import android.content.IntentFilter
-import android.os.Build
-import android.os.IBinder
-import android.os.PowerManager
+import android.hardware.Sensor
+import android.hardware.Sensor.TYPE_PROXIMITY
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.hardware.SensorManager.SENSOR_DELAY_NORMAL
+import android.os.*
 import android.os.PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 
-class JaviService : Service() {
+class JaviService : Service(), SensorEventListener {
     companion object {
         var isStarted = false
         private var receiverRegistered = false
     }
+
+    private val vibrator by lazy { ContextCompat.getSystemService(this, Vibrator::class.java)!! }
+    private val sensorManager by lazy { ContextCompat.getSystemService(this, SensorManager::class.java)!! }
+    private var proximity: Sensor? = null
 
     private val wakeLock by lazy {
         ContextCompat.getSystemService(this, PowerManager::class.java)!!
@@ -39,8 +47,8 @@ class JaviService : Service() {
     private val screenActionReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             when (intent.action) {
-                ACTION_SCREEN_ON -> switchWakelock(true)
-                ACTION_SCREEN_OFF -> switchWakelock(false)
+                ACTION_SCREEN_ON -> switchProximityListener(true)
+                ACTION_SCREEN_OFF -> switchProximityListener(false)
             }
         }
     }
@@ -78,7 +86,6 @@ class JaviService : Service() {
                         .setContentIntent(clickPendingIntent)
                         .build()
         )
-        switchWakelock(true)
 
         val filter = IntentFilter().apply {
             addAction(ACTION_SCREEN_OFF)
@@ -86,6 +93,9 @@ class JaviService : Service() {
         }
         registerReceiver(screenActionReceiver, filter)
         receiverRegistered = true
+
+        proximity = sensorManager.getDefaultSensor(TYPE_PROXIMITY)
+        switchProximityListener(true)
 
         isStarted = true
     }
@@ -108,14 +118,39 @@ class JaviService : Service() {
         }
     }
 
+    private fun switchProximityListener(on: Boolean) {
+        if (on) {
+            proximity?.run {
+                sensorManager.registerListener(this@JaviService, this, SENSOR_DELAY_NORMAL)
+            }
+        } else {
+            sensorManager.unregisterListener(this)
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         switchWakelock(false)
+        switchProximityListener(false)
         if (receiverRegistered) {
             unregisterReceiver(screenActionReceiver)
             receiverRegistered = false
         }
         isStarted = false
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        val distance = event.values[0]
+        if (distance == 0f) {
+            vibrator.vibrate(VibrationEffect.createOneShot(100L, VibrationEffect.DEFAULT_AMPLITUDE))
+            switchWakelock(true)
+        } else {
+            switchWakelock(false)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Do nothing
     }
 
     override fun onBind(intent: Intent): IBinder? = null
